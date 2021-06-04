0c5bc99b4245da58476ed07da4683c042ee1203c
agaikwad07-alt <agaikwad07@gmail.com>
2021-06-04 17:25:46 +0530

    Currency Exchange Service - Microservice

---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into CURRENCY_EXCHANGE
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1001, 65, '', 'USD', 'INR');
+
+insert into CURRENCY_EXCHANGE
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1002, 75, '', 'EUR', 'INR');
+
+insert into CURRENCY_EXCHANGE
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1003, 50, '', 'AUD', 'INR');
